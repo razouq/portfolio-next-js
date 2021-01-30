ef3344e33b6fefc96734e19ec7c41e36f74c4fc8
anass bendarsi <bendarsi@gmail.com>
2021-01-30 23:42:00 +0100

    responsive articles list, adjust small layout padding

--- components/Articles.js
+++ components/Articles.js
@@ -6,7 +6,7 @@ export default function Articles() {
       <h3 className="font-semibold text-3xl text-blue-600 border-b-4 border-blue-600 inline-block pb-1">
         Articles
       </h3>
-      <div className="grid gap-4 grid-cols-3 mt-10">
+      <div className="grid gap-4 md:grid-cols-3 mt-10">
         <Article title="Article 1" />
         <Article title="Article 2" />
         <Article title="Article 3" />
