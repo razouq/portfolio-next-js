0a5837aad97914e1ed19e58a1863b4e6308e8d3a
anass bendarsi <bendarsi@gmail.com>
2021-02-04 22:46:11 +0100

    fix about section

--- components/About.js
+++ components/About.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div className="mt-60">
-      <p className="text-center text-gray-200 text-base sm:text-3xl">
+    <div className="flex-1 flex justify-center items-center">
+      <p className="text-center text-gray-200 text-base sm:text-3xl leading-10 tracking-widest">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
         repudiandae consectetur.
       </p>
--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import About from './About';
 export default function Header() {
   return (
     <div
-    
+      className="flex flex-col"
       style={{
         // background: 'url("/images/hero.jpg") no-repeat, linear-gradient(135deg, #50A684 30%, #115E67 90%)',
         backgroundImage: 'linear-gradient(to right, rgba(17, 24, 39, .5), rgba(17, 24, 39, .5)), url("/images/hero.jpg")',
